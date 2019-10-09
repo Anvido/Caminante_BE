b4ef04b6486da274df7722aaba9939ba1f10dfc9
Raul Coral <rfcoralj@unal.edu.co>
2019-10-09 12:55:35 -0500

    Adding connect test route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const passport = require('./auth/passport')
 const graphqlHTTP = require('express-graphql')
 const { api, auth_controller } = require('./controllers')
 
-
 const app = express()
 const port = process.env.PORT || 3000
 require('./db')
@@ -15,10 +14,11 @@ app.use(morgan('tiny'))
 app.use(express.json())
 
 app.use(auth_controller)
+app.get('/connect', (req, res, next) => {
+  res.status(200).send('1');
+})
 app.use('/test', graphqlHTTP(api))
 app.use('/', passport.authenticate('jwt', { session: false }), graphqlHTTP(api))
-
-
 
 app.listen(port, () => {
   console.log(`Running app on port ${port}`);
